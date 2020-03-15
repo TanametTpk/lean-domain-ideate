79223136a3764adc9a92f7390f68fb282682760e
tanamet.t <tanamet.tpk@gmail.com>
2020-03-16 00:17:39 +0700

    Updates

---
+++ precache-manifest.a3be0faa472072ec081876c4160fec11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89fa70ffdc8cf1c27070106ce4fb929",
+    "url": "/lean-domain-ideate/index.html"
+  },
+  {
+    "revision": "c37c993eb2e0f43d76af",
+    "url": "/lean-domain-ideate/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "6a677549bab53b122b58",
+    "url": "/lean-domain-ideate/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c37c993eb2e0f43d76af",
+    "url": "/lean-domain-ideate/static/js/2.7dad070c.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/lean-domain-ideate/static/js/2.7dad070c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a677549bab53b122b58",
+    "url": "/lean-domain-ideate/static/js/main.e2528c70.chunk.js"
+  },
+  {
+    "revision": "4d2a2632f81302d68b7f",
+    "url": "/lean-domain-ideate/static/js/runtime-main.6755a51b.js"
+  }
+]);
